f156293f5794a04def0eae79e8029cd09b0a4dcb
djordjijem <djordjijem@gmail.com>
2019-03-06 00:40:15 +0100

    Updates

---
+++ precache-manifest.5b6d23c45bea87ea127bdfbfa89f375b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "65c04acb676b6ac23364",
+    "url": "/my-app/static/js/main.65c04acb.chunk.js"
+  },
+  {
+    "revision": "e4dbdbdb96ad72273a9b",
+    "url": "/my-app/static/js/2.e4dbdbdb.chunk.js"
+  },
+  {
+    "revision": "65c04acb676b6ac23364",
+    "url": "/my-app/static/css/main.cc67c88f.chunk.css"
+  },
+  {
+    "revision": "f0b8ac0ba19426b2ac24f651a49c2671",
+    "url": "/my-app/index.html"
+  }
+];
